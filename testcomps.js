11048333f6415c93b5ab0c56df1cb6708c37bb78
Greg Knapp <68096855+gsknapp@users.noreply.github.com>
2026-07-15 12:26:38 -0400

    Pre-transpile commit
    Ran all setup and updates prior to attempting to transpile to python.

---
+++ testcomps.js
@@ -0,0 +1,19 @@
+import {calculate, Generations, Pokemon, Move} from '@smogon/calc';
+
+const gen = Generations.get(9); // alternatively: const gen = 5;
+const result = calculate(
+  gen,
+  new Pokemon(gen, 'Gengar', {
+    item: 'Choice Specs',
+    nature: 'Timid',
+    evs: {spa: 252},
+    boosts: {spa: 1},
+  }),
+  new Pokemon(gen, 'Chansey', {
+    item: 'Eviolite',
+    nature: 'Calm',
+    evs: {hp: 252, spd: 252},
+  }),
+  new Move(gen, 'Focus Blast')
+);
+console.log(result.range())
